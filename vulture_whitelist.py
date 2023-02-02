d67f51daa9db2ef3032f0f9a51b1b8f5734032d3
Quentin Kaiser <quentin.kaiser@onekey.com>
2023-02-02 14:24:59 +0100

    chore: fixup vulture checks.

---
+++ vulture_whitelist.py
@@ -0,0 +1,22 @@
+# TODO: dead code parts must be removed in the future
+from jefferson.core import (
+    JFFS2_COMPAT_MASK,
+    JFFS2_COMPR_COPY,
+    JFFS2_COMPR_DYNRUBIN,
+    JFFS2_COMPR_RUBINMIPS,
+    JFFS2_FEATURE_ROCOMPAT,
+    JFFS2_FEATURE_RWCOMPAT_COPY,
+    data_crc_match,
+    name_crc_match,
+    node_crc_match,
+)
+
+data_crc_match
+name_crc_match
+node_crc_match
+JFFS2_COMPR_RUBINMIPS
+JFFS2_COMPR_COPY
+JFFS2_COMPR_DYNRUBIN
+JFFS2_COMPAT_MASK
+JFFS2_FEATURE_ROCOMPAT
+JFFS2_FEATURE_RWCOMPAT_COPY
